12f7f8727b8efdfc85fb31a74971b4d8ea3a4722
EU\UbedaMuN <nubeda@enaire.es>
2023-01-16 18:43:28 +0100

    se mueven ficheros

---
+++ src/main/java/com/unisys/ejercicios/example/Ejercicio01.java
@@ -0,0 +1,16 @@
+package com.unisys.ejercicios.example;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        int numero = 20;
+        short marianico = 20;
+        long gasol = 30;
+        boolean independencia;
+        double presupuesto;
+        float longitud;
+        long suma = gasol + marianico;
+        System.out.println("la suma de gasol y marianico es: "+suma);
+
+    }
+
+}
